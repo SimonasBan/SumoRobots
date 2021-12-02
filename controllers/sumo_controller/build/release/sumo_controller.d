7683c19ea6010823608630c20f4c5ccaab1ee312
SimonasBan <sbansevicius@gmail.com>
2021-12-02 19:04:37 +0200

    Project setup

---
+++ controllers/sumo_controller/build/release/sumo_controller.d
@@ -0,0 +1,5 @@
+build/release/sumo_controller.o: sumo_controller.c \
+ D:\Program\ Files\Webots/include/controller/c/webots/robot.h \
+ D:\Program\ Files\Webots/include/controller/c/webots/types.h \
+ D:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
+ D:\Program\ Files\Webots/include/controller/c/webots/motor.h
